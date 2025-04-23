79c7794a625000e1b9f313cded0327d579e35a7b
zihan_cityu <32484940+wzh4464@users.noreply.github.com>
2025-04-23 11:25:43 +0800

    添加子模块配置与姓名字典数据
    - 忽略 *.gz, *.png 和 *.npy 文件
    - 添加 threeDCSQ 子模块，并更新 .gitmodules
    - 配置 VSCode 调试（launch.json）和分析路径（settings.json）
    - 新增 DATA/name_dictionary.csv 作为姓名映射字典

---
+++ __init__.py
@@ -0,0 +1,13 @@
+###
+# File: ./SegCell/__init__.py
+# Created Date: Tuesday, April 8th 2025
+# Author: Zihan
+# -----
+# Last Modified: Tuesday, 8th April 2025 12:30:10 am
+# Modified By: the developer formerly known as Zihan at <wzh4464@gmail.com>
+# -----
+# HISTORY:
+# Date      		By   	Comments
+# ----------		------	---------------------------------------------------------
+###
+
